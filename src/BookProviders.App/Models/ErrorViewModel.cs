0b7b60d574a8212c561c12482391ee19ca1a6d5e
Paula Zaidel <paulazaaidel@gmail.com>
2019-10-02 08:46:33 -0300

    Init Project

---
+++ src/BookProviders.App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookProviders.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
