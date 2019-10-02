c7370094e40cbf8f25bfa1e3cd359653006314c4
Paula Zaidel <paulazaaidel@gmail.com>
2019-10-02 10:08:45 -0300

    Create mappings

--- src/BookProviders.Data/Context/BookProvidersContext.cs
+++ src/BookProviders.Data/Context/BookProvidersContext.cs
@@ -1,6 +1,7 @@
 ﻿
 using BookProviders.Business.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace BookProviders.Data.Context
 {
@@ -11,5 +12,24 @@ namespace BookProviders.Data.Context
         public DbSet<Product> Products { get; set; }
         public DbSet<Caterer> Caterers { get; set; }
         public DbSet<Address> Adresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure mvarchar to varchar
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                .Where(p => p.ClrType == typeof(string))))
+                property.Relational().ColumnType = "varchar(100)";
+
+            // Configure Mappings
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BookProvidersContext).Assembly);
+
+            // Turn Off delete cascade
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys()))
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ src/BookProviders.Data/Mappings/CatererMapping.cs
@@ -0,0 +1,34 @@
+﻿
+using BookProviders.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookProviders.Data.Mappings
+{
+    public class CatererMapping : IEntityTypeConfiguration<Caterer>
+    {
+        public void Configure(EntityTypeBuilder<Caterer> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(c => c.Document)
+                .IsRequired()
+                .HasColumnType("varchar(14)");
+
+            //1:1 => Caterer : Adress
+            builder.HasOne(c => c.Adress)
+                .WithOne(a => a.Caterer);
+
+            //1:N => Caterer : Products
+            builder.HasMany(c => c.Products)
+                .WithOne(p => p.Caterer)
+                .HasForeignKey(p => p.CatererId);
+
+            builder.ToTable("Caterers");
+        }
+    }
+}
