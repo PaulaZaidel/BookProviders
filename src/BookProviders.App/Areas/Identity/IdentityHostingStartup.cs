4e34b0b09086a7e232b94fa26062194456d66f63
Paula Zaidel <paulazaaidel@gmail.com>
2019-10-21 08:14:26 -0300

    Scaffolding identity pages

---
+++ src/BookProviders.App/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BookProviders.App.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BookProviders.App.Areas.Identity.IdentityHostingStartup))]
+namespace BookProviders.App.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
